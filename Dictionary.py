b186e76d2233c2392893e57b3064cc56c19b7869
ramhemanth3 <ram.hemanth3@gmail.com>
2020-02-15 22:26:30 +0530

    Update Dictionary.py
    Added Synonym Antonym

--- Dictionary.py
+++ Dictionary.py
@@ -3,18 +3,34 @@ from PyDictionary import PyDictionary
 
 ab = Tk()
 ab.geometry('400x300')
-meaning=[""]
+meaning=""
 def find_Meaning():
     word=ent.get()
     dictionary= PyDictionary(word)
-    meaning[] = dictionary.printMeanings()
-    print(meaning[])    
+    meaning = str( dictionary.printMeanings())
+    print(meaning)    
+
+def find_Synonym():
+    word=ent.get()
+    print("Synonym is ")
+    dictionary= PyDictionary(word)
+    print(dictionary.synonym(word))
+
+def find_Antonym():
+    word=ent.get()
+    print("Antonym is ")
+    dictionary= PyDictionary(word)
+    print(dictionary.antonym(word))    
         
     
 ent = Entry(ab, font=("times", 17))
 ent.grid(row=2,column=2)
 btn = Button(ab, text = "Meaning",command=find_Meaning)
-btn.grid(row=4,column=2)
-labelMeaning = Label(ab,text=meaning[]).grid(row=5, column=0)
+btn.grid(row=4,column=1)
+btn2 = Button(ab, text = "Synonym",command=find_Synonym)
+btn2.grid(row=4,column=2)
+btn3 = Button(ab, text = "Antonym",command=find_Antonym)
+btn3.grid(row=4,column=3)
+labelMeaning = Label(ab,text=meaning).grid(row=5, column=0)
 
 ab.mainloop()
